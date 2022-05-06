d379b552928b6d83285257934bcd6fc43cbe9651
YangQing-Lin <1665486570@qq.com>
2022-05-06 14:07:21 +0800

    post to acapp

--- scripts/compress_game_js.sh
+++ scripts/compress_game_js.sh
@@ -1,7 +1,15 @@
 #! /bin/bash
 
+
 JS_PATH=/home/bob/AcApp-BattleOfBalls/game/static/js/
 JS_PATH_DIST=${JS_PATH}dist/
 JS_PATH_SRC=${JS_PATH}src/
 
+# 合并代码
 find . $JS_PATH_SRC -type f -name '*.js' | sort | xargs cat > ${JS_PATH_DIST}game.js
+
+# 同步静态文件
+echo yes | python3 /home/bob/AcApp-BattleOfBalls/manage.py collectstatic
+
+# 启动uwsgi服务
+uwsgi --ini /home/bob/AcApp-BattleOfBalls/scripts/uwsgi.ini
