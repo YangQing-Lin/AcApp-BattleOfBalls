4549859db7f6d319909c2aa40d924cfc2bd5ef41
YangQing-Lin <1665486570@qq.com>
2022-05-09 17:22:11 +0800

    Implemented Fireball cooldown, icon and mask

--- game/static/js/src/playground/player/zbase.js
+++ game/static/js/src/playground/player/zbase.js
@@ -23,12 +23,20 @@ class Player extends AcGameObject {
         this.spent_time = 0;
         this.enemy_cold_time = 3;  // 敌人3秒之后开始战斗
         this.fireballs = [];  // 自己发出的所有子弹
+        this.base_fireball_coldtime = 1;
 
         this.cur_skill = null;
 
         if (this.character !== "robot") {
             this.img = new Image();
             this.img.src = this.photo;
+        }
+
+        if (this.character === "me") {
+            // 火球技能冷却时间（单位：秒）
+            this.fireball_coldtime = this.base_fireball_coldtime;
+            this.fireball_img = new Image();
+            this.fireball_img.src = "https://cdn.acwing.com/media/article/image/2021/12/02/1_9340c86053-fireball.png";
         }
     }
 
@@ -83,8 +91,9 @@ class Player extends AcGameObject {
             } else if (e.which === 1) {
                 let tx = (e.clientX - rect.left) / outer.playground.scale;
                 let ty = (e.clientY - rect.top) / outer.playground.scale;
-                if (outer.cur_skill === "fireball") {
+                if (outer.cur_skill === "fireball" && outer.fireball_coldtime <= outer.eps) {
                     let fireball = outer.shoot_fireball(tx, ty);
+                    console.log("shoot fireball");
 
                     // 如果是多人模式就广播发射火球的行为
                     if (outer.playground.mode === "multi mode") {
@@ -92,6 +101,7 @@ class Player extends AcGameObject {
                     }
                 } else {
                     let fireball = outer.shoot_fireball(tx, ty);
+                    console.log("shoot bullet");
 
                     if (outer.playground.mode === "multi mode") {
                         outer.playground.mps.send_shoot_fireball(tx, ty, fireball.uuid);
@@ -110,6 +120,12 @@ class Player extends AcGameObject {
                 return false;
             }
 
+            // 技能冷却好之前不能放
+            if (outer.fireball_coldtime > outer.eps) {
+                return false;
+            }
+
+            // Q技能
             if (e.which === 81) {
                 outer.cur_skill = "fireball";
                 return false;
@@ -129,6 +145,8 @@ class Player extends AcGameObject {
         let fireball = new FireBall(this.playground, this, x, y, radius, vx, vy, color, speed, move_length, 0.01);
         // 将新生成的火球放进自己的火球数组里
         this.fireballs.push(fireball);
+
+        this.fireball_coldtime = this.base_fireball_coldtime;
 
         // 返回刚刚发射的火球（用于在room里同步所有子弹的uuid）
         return fireball;
@@ -199,7 +217,19 @@ class Player extends AcGameObject {
 
     update() {
         this.update_move();
+
+        // 只有自己，并且在fighting状态下才更新冷却时间
+        if (this.character === "me" && this.playground.state === "fighting") {
+            this.update_codetime();
+        }
+
         this.render();
+    }
+
+    // 更新技能冷却时间
+    update_codetime() {
+        this.fireball_coldtime -= this.timedelta / 1000;
+        this.fireball_coldtime = Math.max(this.fireball_coldtime, 0);
     }
 
     // 更新玩家移动
@@ -261,6 +291,35 @@ class Player extends AcGameObject {
             this.ctx.beginPath();
             this.ctx.arc(this.x * scale, this.y * scale, this.radius * scale, 0, Math.PI * 2, false);
             this.ctx.fillStyle = this.color;
+            this.ctx.fill();
+        }
+
+        if (this.character === "me" && this.playground.state === "fighting") {
+            this.render_skill_coldtime();
+        }
+    }
+
+    // 渲染技能图标和冷却时间蒙版
+    render_skill_coldtime() {
+        let x = 1.5, y = 0.9, r = 0.04;
+        let scale = this.playground.scale;
+
+        // 渲染图片
+        this.ctx.save();
+        this.ctx.beginPath();
+        this.ctx.arc(x * scale, y * scale, r * scale, 0, Math.PI * 2, false);
+        this.ctx.stroke();
+        this.ctx.clip();
+        this.ctx.drawImage(this.fireball_img, (x - r) * scale, (y - r) * scale, r * 2 * scale, r * 2 * scale);
+        this.ctx.restore();
+
+        // 渲染剩余冷却时间蒙版
+        if (this.fireball_coldtime > 0) {
+            this.ctx.beginPath();
+            this.ctx.moveTo(x * scale, y * scale);
+            this.ctx.arc(x * scale, y * scale, r * scale, 0 - Math.PI / 2, Math.PI * 2 * (1 - this.fireball_coldtime / this.base_fireball_coldtime) - Math.PI / 2, true);
+            this.ctx.lineTo(x * scale, y * scale);
+            this.ctx.fillStyle = "rgba(0, 0, 255, 0.4)";
             this.ctx.fill();
         }
     }
